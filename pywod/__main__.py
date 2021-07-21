363cd63d461d26048d2a5f0b1971d4df30bb0117
Vivaan Parashar <parashar.xtra@gmail.com>
2021-07-21 18:14:24 +0530

    update dependencies and add base code

--- pywod/__main__.py
+++ pywod/__main__.py
@@ -1,2 +1,16 @@
-def main():
-    print("WOD")
+from pptx import Presentation
+
+
+def main(word: str):
+    # args = ()
+    prs = Presentation()
+    title_slide = prs.slides.add_slide(prs.slide_layouts[0])
+    title = title_slide.shapes.title
+    subtitle = title_slide.placeholders[1]
+
+    title.text = "test"
+    subtitle.text = "1234"
+
+    # definition_slide = prs.slides.add_slide()
+
+    prs.save(f"{word.replace(' ', '_')}.pptx")
